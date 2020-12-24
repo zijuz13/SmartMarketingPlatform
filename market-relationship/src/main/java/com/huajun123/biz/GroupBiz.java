c8291ecbf77659e2074b1fd4aeee894e16eeb4aa
zijuz13 <zijuz13@uci.edu>
2020-12-24 20:48:26 +0800

    新增GROUPCRUD

--- market-relationship/src/main/java/com/huajun123/biz/GroupBiz.java
+++ market-relationship/src/main/java/com/huajun123/biz/GroupBiz.java
@@ -1,7 +1,9 @@
 package com.huajun123.biz;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.huajun123.entity.Contact;
 import com.huajun123.entity.Group;
+import com.huajun123.mappers.ContactMapper;
 import com.huajun123.utils.GroupRequest;
 import com.huajun123.utils.SearchResult;
 import org.apache.commons.lang.StringUtils;
@@ -13,12 +15,15 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.entity.Example;
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class GroupBiz extends BaseBiz<Group> implements IGroupBiz{
     @Autowired
     private BeanFactory factory;
+    @Autowired
+    private ContactMapper mapper1;
     @PostConstruct
     public void construct(){
         this.setMapper((Mapper<Group>)factory.getBean("groupMapper"));
@@ -31,7 +36,19 @@ public class GroupBiz extends BaseBiz<Group> implements IGroupBiz{
         group.setUpdator(null);
         return super.createItem(group);
     }
-
+    //
+    private String analyzeCorrespondingContacts(String con){
+        //Get all the ocntacts
+        List<Contact> contacts = mapper1.selectAll();
+        //Select the contacters that are corresponding to the conditions.
+        List<Long> collect = contacts.stream().filter(contact -> {
+            boolean name;
+            boolean
+            for (String s : con.split(";")) {
+                if(s.contains())
+            }
+        }).map(Contact::getGuid).collect(Collectors.toList());
+    }
     @Override
     public int updateItem(Group group) {
         group.setImporttime(null);
